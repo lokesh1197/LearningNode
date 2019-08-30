2907e2e738ff5149598657762730b88c9ade11d9
Lokesh Mohanty <lokesh1197@gmail.com>
2019-08-30 17:35:25 +0530

    Add typescript learning material

---
+++ Typescript/README.md
@@ -0,0 +1,13 @@
+Install Typescript:
+  - `npm install -g typescript`
+
+Run typescript file:
+  - `tsc <filename>`
+  - It creates a .js file from the .ts file
+  - Extension of the ts file is '.ts'
+
+Files created:
+  - `greeter.js` is the .js file created from `greeter.ts`
+
+Documentation:
+  - [https://www.typescriptlang.org/docs/home.html]
---
+++ Typescript/greeter.ts
@@ -0,0 +1,28 @@
+class Student {
+  fullname: string;
+  constructor(public firstname: string, public middlename: string, public lastname: string) {
+    this.fullname = firstname + " " + middlename + " " + lastname;
+  }
+}
+// 'public' automatically creates properties with the argument's name
+
+interface Person {
+  firstname: string;
+  lastname: string; 
+}
+
+function greet(person: Person) {
+  return "Hello, " + person.firstname + " " + person.lastname;  
+}
+function greeter(name: string) {
+  return "Hello " + name;
+}
+
+let user = "Lokesh";
+let userNo = 1234;
+let student = new Student("Lokesh", "", "Mohanty");
+
+console.log(student);
+console.log(greeter(user));
+console.log(greeter(userNo));
+console.log(greet({firstname: "Lokesh", lastname: "Mohanty"}));
